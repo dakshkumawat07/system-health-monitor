dd35ec3afeb44977f3ed2378d3d9f8d3a0965ec9
Daksh Kumawat <dakshkumawat239@gmail.com>
2026-08-18 06:30:18 -0400

    feat: add logical CPU information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import platform
+import os
+
 
 print("========================================")
 print("       SYSTEM HEALTH MONITOR")
@@ -8,6 +10,7 @@ print()
 print("Operating System :", platform.system())
 print("System           :", platform.release())
 print("Machine          :", platform.machine())
+print("Logical CPUs     :", os.cpu_count())
 print("Processor        :", platform.processor())
 print("Python Version   :", platform.python_version())
 
